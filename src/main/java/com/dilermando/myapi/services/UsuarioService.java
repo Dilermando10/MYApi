f73fb329a8c76d253c4020614f1e77d96b4d44af
Dilermando <dilermando10@gmail.com>
2022-01-25 16:56:05 -0300

    Criado o Recurso de atualização - Put

--- src/main/java/com/dilermando/myapi/services/UsuarioService.java
+++ src/main/java/com/dilermando/myapi/services/UsuarioService.java
@@ -12,23 +12,28 @@ import com.dilermando.myapi.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class UsuarioService {
-	
-	
+
 	@Autowired
 	private UsuarioRepository repository;
-	
-	
+
 	public Usuario findById(Integer id) {
-		
+
 		Optional<Usuario> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado id: " + id + ", tipo:" + Usuario.class.getName()));
-				
-	}
 
+	}
 
 	public List<Usuario> findAll() {
 		return repository.findAll();
+	}
+
+	public Usuario update(Integer id, Usuario obj) {
+		Usuario newObj = findById(id);
+		newObj.setNome(obj.getNome());
+		newObj.setLogin(obj.getLogin());
+		newObj.setSenha(obj.getSenha());
+		return repository.save(newObj);
 	}
 
 }
