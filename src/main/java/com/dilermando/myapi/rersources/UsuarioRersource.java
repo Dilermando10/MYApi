7fb65d7a1f200b72ff1bc32187a52077b1c71884
Dilermando <dilermando10@gmail.com>
2022-01-25 17:27:19 -0300

    Criado o Recurso de Deletar um usuário

--- src/main/java/com/dilermando/myapi/rersources/UsuarioRersource.java
+++ src/main/java/com/dilermando/myapi/rersources/UsuarioRersource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,6 @@ public class UsuarioRersource {
 	public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
 		Usuario obj = this.service.findById(id);
 		return ResponseEntity.ok().body(obj);
-
 	}
 
 	@GetMapping
@@ -51,4 +51,12 @@ public class UsuarioRersource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
+	}
+
 }
